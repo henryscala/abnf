5722ea33f204930e686a9ad3eb0906773ad7796a
henryscala <hongxinx@gmail.com>
2016-01-11 22:29:32 +0800

    study ambiguous

--- parsec/parsec.go
+++ parsec/parsec.go
@@ -2,65 +2,233 @@
 package parsec
 
 import (
+	"fmt"
 	"strings"
+	"unicode/utf8"
 )
 
-var EmptyStringSlice []string = []string{}
-var EmptyAnySlice []interface{} = []interface{}{}
+type Parser func(input string) (result string, remaining string, ok bool)
 
-type Parser func(input string) (result []string, remaining string)
-
-type AppliedParser func(input string) (result []interface{}, remaining string)
+type AppliedParser func(input string) (result interface{}, remaining string, ok bool)
 
 type Apply func(inbound string) interface{}
 
+func Success() Parser {
+	return func(input string) (result string, remaining string, ok bool) {
+		return "", input, true
+	}
+}
+
+func Fail() Parser {
+	return func(input string) (result string, remaining string, ok bool) {
+		return "", input, false
+	}
+}
+
 func Literal(s string) Parser {
-	return func(input string) (result []string, remaining string) {
+	return func(input string) (result string, remaining string, ok bool) {
 
 		if !strings.HasPrefix(input, s) {
-			return EmptyStringSlice, input
+			return "", input, false
 		}
-		result = append(result, s)
+		result = s
 		remaining = input[len(s):]
+		ok = true
 		return
 	}
 }
 
-func Alternative(p1, p2 Parser) Parser {
-	return func(input string) (result []string, remaining string) {
-		result, remaining = p1(input)
-		if len(result) != 0 {
+func Set(f func(string) bool) Parser {
+	return func(input string) (result string, remaining string, ok bool) {
+		r, size := utf8.DecodeRuneInString(input)
+		if r == utf8.RuneError {
+			return "", input, false
+		}
+		if !f(string(r)) {
+			return "", input, false
+		}
+		return string(r), input[size:], true
+	}
+}
+
+func isDigit(s string) bool {
+	if len(s) != 1 {
+		panic("not exptected")
+	}
+	if s[0] >= '0' && s[0] <= '9' {
+		return true
+	}
+	return false
+}
+
+func isLower(s string) bool {
+	if len(s) < 1 {
+		panic("not exptected")
+	}
+	if s[0] >= 'a' && s[0] <= 'z' {
+		return true
+	}
+	return false
+}
+
+func isUpper(s string) bool {
+	if len(s) < 1 {
+		panic("not exptected")
+	}
+	if s[0] >= 'A' && s[0] <= 'Z' {
+		return true
+	}
+	return false
+}
+
+func UpperLetter() Parser {
+	return Set(isUpper)
+}
+
+func LowerLetter() Parser {
+	return Set(isLower)
+}
+
+func Letter() Parser {
+	return Alternative(LowerLetter(), UpperLetter())
+}
+
+func Digit() Parser {
+	return Set(isDigit)
+}
+
+func AnyChar() Parser {
+	return Set(func(string) bool {
+		return true //always
+	})
+}
+
+func Alternative(ps ...Parser) Parser {
+	p := Fail()
+	for _, p1 := range ps {
+
+		p = alternative2(p, p1)
+	}
+	return p
+
+}
+
+func alternative2(p1, p2 Parser) Parser {
+	return func(input string) (result string, remaining string, ok bool) {
+		result, remaining, ok = p1(input)
+		if ok { //succeed
 			return
 		}
-		result, remaining = p2(input)
+		result, remaining, ok = p2(input)
 		return
 	}
 }
 
-func Concat(p1, p2 Parser) Parser {
-	return func(input string) (result []string, remaining string) {
-		result, remaining = p1(input)
-		if len(result) == 0 { //failed
-			return EmptyStringSlice, input
+func Concat(ps ...Parser) Parser {
+	p := Success()
+	for _, p1 := range ps {
+		p = concat2(p, p1)
+	}
+	return p
+}
+
+func concat2(p1, p2 Parser) Parser {
+	return func(input string) (result string, remaining string, ok bool) {
+		result, remaining, ok = p1(input)
+		if !ok { //failed
+			return "", input, false
 		}
-		result2, remaining2 := p2(remaining)
-		if len(result2) == 0 {
-			return EmptyStringSlice, input
+		result2, remaining2, ok2 := p2(remaining)
+		if !ok2 {
+			return "", input, false
 		}
 
-		result[0] += result2[0]
-		return result, remaining2
+		return result + result2, remaining2, true
 	}
 }
 
 func Map(f Apply, p Parser) AppliedParser {
-	return func(input string) (result []interface{}, remaining string) {
-		var result1 []string
-		result1, remaining = p(input)
-		if len(result1) == 0 {
-			return EmptyAnySlice, input
+	return func(input string) (result interface{}, remaining string, ok bool) {
+		var result1 string
+		result1, remaining, ok = p(input)
+		if !ok {
+			return "", input, false
 		}
-		result = append(result, f(result1[0]))
-		return result, remaining
+		result = f(result1)
+
+		return result, remaining, true
+	}
+}
+
+//a?
+func RepeatZeroOne(p Parser) Parser {
+	return Repeat(p, 0, 1)
+}
+
+//a+
+func RepeatOneMore(p Parser) Parser {
+	return Repeat(p, 1, -1)
+}
+
+//a*
+func RepeatZeroMore(p Parser) Parser {
+	return Repeat(p, 0, -1)
+}
+
+func Repeat(p Parser, min, max int) Parser {
+	if max >= 0 {
+		if max < min {
+			panic("not expected")
+		}
+	} else {
+		// max < 0 means infinity
+	}
+	return func(input string) (result string, remaining string, ok bool) {
+		ok = true
+		result1, remaining1, ok1 := result, remaining, ok
+		input1 := input
+		for i := 0; i < min; i++ {
+			result1, remaining1, ok1 = p(input1)
+			if !ok1 {
+				fmt.Println("Repeat1-", min, max, "", input, false)
+				return "", input, false
+			}
+			input1 = remaining1
+			result += result1
+		}
+
+		for i := min; i < max; i++ {
+			result1, remaining1, ok1 = p(input1)
+			if !ok1 {
+				fmt.Println("Repeat2-", min, max, result, input1, true)
+				return result, input1, true //return true up till now
+			}
+			input1 = remaining1
+			result += result1
+		}
+
+		if max >= 0 {
+			remaining = remaining1
+			fmt.Println("Repeat3-", min, max, result, remaining, true)
+			return result, remaining, true //return true up till now
+		}
+
+		//max < 0 means infinity
+		for i := min; true; i++ {
+			if len(input1) == 0 {
+				break
+			}
+			result1, remaining1, ok1 = p(input1)
+			fmt.Println("Repat3.5-", "result1", result1, "remaining1", remaining1, "ok1", ok1)
+			if !ok1 {
+				fmt.Println("Repeat4-", min, max, result, input1, true)
+				return result, input1, true //return true up till now
+			}
+			input1 = remaining1
+			result += result1
+		}
+
+		fmt.Println("Repeat5-", min, max, result, input1, true)
+		return result, input1, true
 	}
 }
--- parsec/parsec_test.go
+++ parsec/parsec_test.go
@@ -5,13 +5,78 @@ import (
 	"testing"
 )
 
+func TestParsecAnyChar(t *testing.T) {
+	input := "abc"
+	parser := Concat(AnyChar(), AnyChar())
+	result, remaining, ok := parser(input)
+	t.Log("result", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+	if result != "ab" {
+		t.Error("nok")
+	}
+	if remaining != "c" {
+		t.Error("nok")
+	}
+}
+
+func TestParsecDigit(t *testing.T) {
+	input := "a1"
+	parser := Digit()
+	result, remaining, ok := parser(input)
+	t.Log("result", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if ok {
+		t.Error("nok")
+	}
+
+	input = "1a"
+	parser = Digit()
+	result, remaining, ok = parser(input)
+	t.Log("result", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+}
+
+func TestParsecLetter(t *testing.T) {
+	input := "a1"
+	parser := Letter()
+	result, remaining, ok := parser(input)
+	t.Log("result", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+
+	input = "1a"
+	parser = Letter()
+	result, remaining, ok = parser(input)
+	t.Log("result", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if ok {
+		t.Error("nok")
+	}
+}
+
 func TestParsecLiteral(t *testing.T) {
 	input := "1+2+3"
 	parser := Literal("1+")
-	result, remaining := parser(input)
+	result, remaining, ok := parser(input)
 	t.Log("result", result)
 	t.Log("remaining", remaining)
-
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
 }
 
 func TestParsecAlternative(t *testing.T) {
@@ -20,9 +85,13 @@ func TestParsecAlternative(t *testing.T) {
 	parser2 := Literal("a")
 	parser := Alternative(parser1, parser2)
 
-	result, remaining := parser(input)
+	result, remaining, ok := parser(input)
 	t.Log("result", result)
 	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
 }
 
 func TestParsecConcat(t *testing.T) {
@@ -31,9 +100,13 @@ func TestParsecConcat(t *testing.T) {
 	parser2 := Literal("b")
 	parser := Concat(parser1, parser2)
 
-	result, remaining := parser(input)
+	result, remaining, ok := parser(input)
 	t.Log("result", result)
 	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
 }
 
 func TestParsecApply(t *testing.T) {
@@ -48,7 +121,85 @@ func TestParsecApply(t *testing.T) {
 
 	parser := Map(toInt, parser1)
 
-	result, remaining := parser(input)
-	t.Log("result is int", result[0].(int))
+	result, remaining, ok := parser(input)
+	t.Log("result is int", result.(int))
 	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+}
+
+func TestRepeatZeroOne(t *testing.T) {
+	input := "abc"
+	parser1 := Literal("a")
+	parser := RepeatZeroOne(parser1)
+	result, remaining, ok := parser(input)
+	t.Log("result is ", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+
+	input = "bbc"
+	parser1 = Literal("a")
+	parser = RepeatZeroOne(parser1)
+	result, remaining, ok = parser(input)
+	t.Log("result is ", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+}
+
+func TestRepeatZeroMore(t *testing.T) {
+	input := "abc"
+
+	parser := RepeatZeroMore(AnyChar())
+	result, remaining, ok := parser(input)
+	t.Log("result is ", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+	if result != "abc" {
+		t.Error("nok")
+	}
+}
+
+func TestRepeatOneMore(t *testing.T) {
+	input := "123a"
+
+	parser := RepeatOneMore(Digit())
+	result, remaining, ok := parser(input)
+	t.Log("result is ", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
+	if result != "123" {
+		t.Error("nok")
+	}
+	if remaining != "a" {
+		t.Error("nok")
+	}
+}
+
+func TestRepeatAmbiguous(t *testing.T) {
+	input := "a123"
+	parser1 := RepeatZeroMore(AnyChar())
+	parser2 := RepeatOneMore(Digit())
+
+	parser := Concat(parser1, parser2)
+	result, remaining, ok := parser(input)
+	t.Log("result is ", result)
+	t.Log("remaining", remaining)
+	t.Log("ok", ok)
+	if !ok {
+		t.Error("nok")
+	}
 }
